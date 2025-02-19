928e3970442733477dd15d6ccf1b6f1d6c5ff8db
jaime.gonbra <jaime.gonbra@educa.jcyl.es>
2025-02-19 09:25:07 +0100

    intento de hacer cosas con los autores

--- src/main/java/com/example/tfglibraryofohara/Controllers/AutorController.java
+++ src/main/java/com/example/tfglibraryofohara/Controllers/AutorController.java
@@ -1,8 +1,14 @@
 package com.example.tfglibraryofohara.Controllers;
 
 import com.example.tfglibraryofohara.Services.AutorServicio;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutorController {
     @Autowired
     private AutorServicio autorServicio;
+
+
+    @GetMapping("/todos")
+    @Operation(summary = "listar todos los Autores (si hay)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",)
+    })
+    public ResponseEntity<?> listarTodos() {
+        return autorServicio.listarTodos().size() > 0 ?
+                new ResponseEntity<>(autorServicio.listarTodos(), HttpStatus.OK) : // 200
+                new ResponseEntity<>("No hay autores en la bd", HttpStatus.NO_CONTENT);//204
+
+    }
 }
--- src/main/java/com/example/tfglibraryofohara/Services/AutorServicio.java
+++ src/main/java/com/example/tfglibraryofohara/Services/AutorServicio.java
@@ -1,11 +1,18 @@
 package com.example.tfglibraryofohara.Services;
 
+import com.example.tfglibraryofohara.Models.Autor;
 import com.example.tfglibraryofohara.Repositorys.AutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AutorServicio {
     @Autowired
     private AutorRepository autorRepository;
+
+    public List<Autor> listarTodos() {
+        return autorRepository.findAll();
+    }
 }
